b9204470c67812fd260f3ed1b11b9d46b2a8d629
duckception <spidiritsu@gmail.com>
2023-07-22 13:41:15 +0200

    WIP - webapp

---
+++ packages/telegram-webapp/src/app/ethersRPC.ts
@@ -0,0 +1,123 @@
+import type { SafeEventEmitterProvider } from "@web3auth/base";
+import { ethers } from "ethers";
+import { createWalletClient, custom } from "viem"
+import { sepolia } from 'viem/chains'
+
+export default class EthereumRpc {
+    private provider: SafeEventEmitterProvider;
+
+    constructor(provider: SafeEventEmitterProvider) {
+        this.provider = provider;
+    }
+
+    async getChainId(): Promise<any> {
+        try {
+            const ethersProvider = new ethers.providers.Web3Provider(this.provider);
+            // Get the connected Chain's ID
+            const networkDetails = await ethersProvider.getNetwork();
+
+            const client = createWalletClient({
+                chain: sepolia,
+                transport: custom(this.provider)
+            })
+
+            const chainId = await client.getChainId()
+            console.log({ chainId })
+
+            const accounts = await client.getAddresses()
+            console.log({accounts})
+
+            return networkDetails.chainId;
+        } catch (error) {
+            return error;
+        }
+    }
+
+    async getAccounts(): Promise<any> {
+        try {
+            const ethersProvider = new ethers.providers.Web3Provider(this.provider);
+            const signer = ethersProvider.getSigner();
+
+            // Get user's Ethereum public address
+            const address = await signer.getAddress();
+
+            return address;
+        } catch (error) {
+            return error;
+        }
+    }
+
+    async getBalance(): Promise<string> {
+        try {
+            const ethersProvider = new ethers.providers.Web3Provider(this.provider);
+            const signer = ethersProvider.getSigner();
+
+            // Get user's Ethereum public address
+            const address = await signer.getAddress();
+
+            // Get user's balance in ether
+            const balance = ethers.utils.formatEther(
+                await ethersProvider.getBalance(address) // Balance is in wei
+            );
+
+            return balance;
+        } catch (error) {
+            return error as string;
+        }
+    }
+
+    async sendTransaction(): Promise<any> {
+        try {
+            const ethersProvider = new ethers.providers.Web3Provider(this.provider);
+            const signer = ethersProvider.getSigner();
+
+            const destination = "0xCb2602FabD339eD79BBa7dDB90dCe12Fac2A4948";
+
+            // Convert 1 ether to wei
+            const amount = ethers.utils.parseEther("0.001");
+
+            // Submit transaction to the blockchain
+            const tx = await signer.sendTransaction({
+                to: destination,
+                value: amount,
+                maxPriorityFeePerGas: "5000000000", // Max priority fee per gas
+                maxFeePerGas: "6000000000000", // Max fee per gas
+            });
+
+            // Wait for transaction to be mined
+            const receipt = await tx.wait();
+
+            return receipt;
+        } catch (error) {
+            return error as string;
+        }
+    }
+
+    async signMessage() {
+        try {
+            const ethersProvider = new ethers.providers.Web3Provider(this.provider);
+            const signer = ethersProvider.getSigner();
+
+            const originalMessage = "YOUR_MESSAGE";
+
+            // Sign the message
+            const signedMessage = await signer.signMessage(originalMessage);
+
+            return signedMessage;
+        } catch (error) {
+            return error as string;
+        }
+    }
+
+    async getPrivateKey(): Promise<any> {
+        try {
+            const privateKey = await this.provider.request({
+                method: "eth_private_key",
+            });
+
+            return privateKey;
+        } catch (error) {
+            return error as string;
+        }
+    }
+}
--- packages/telegram-webapp/src/app/page.tsx
+++ packages/telegram-webapp/src/app/page.tsx
@@ -1,95 +1,272 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { useEffect, useState } from "react";
+// HIGHLIGHTSTART-importModules
+import { Web3AuthNoModal } from "@web3auth/no-modal";
+import { CHAIN_NAMESPACES, SafeEventEmitterProvider, WALLET_ADAPTERS } from "@web3auth/base";
+import RPC from "./ethersRPC";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
+
+const clientId = "BNM3ytXW4KY_TE468iF6Qwqc4EIskvJr6gjmCkj5PCXNgaLRz2XRF6HKhDGCknw6n_ROapG-7vqCAbCudMEpY_4"; // get from https://dashboard.web3auth.io
+
+const chainConfig = {
+  chainNamespace: CHAIN_NAMESPACES.EIP155,
+  chainId: "0xaa36a7",
+  rpcTarget: "https://eth-sepolia.g.alchemy.com/v2/C4bpWsBCwk4VvaLDGRhkjUvIpHcIhM7T",
+  // rpcTarget: "https://eth-goerli.g.alchemy.com/v2/IjVFtqiHIPdAGoFJTTf4NR6ol8yxC-Lh",
+  displayName: "Ethereum Sepolia",
+  blockExplorer: "https://goerli.etherscan.io",
+  ticker: "ETH",
+  tickerName: "Ethereum",
+};
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  const [web3auth, setWeb3auth] = useState<Web3AuthNoModal | null>(null);
+  const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(null);
+  const [email, setEmail] = useState<string>('telegram-wallet@proton.me');
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3auth = new Web3AuthNoModal({
+          clientId,
+          chainConfig,
+        });
+
+        const privateKeyProvider = new EthereumPrivateKeyProvider({ config: { chainConfig }});
+
+        const openloginAdapter = new OpenloginAdapter({
+          privateKeyProvider,
+          adapterSettings: {
+            uxMode: "redirect",
+            network: "testnet"
+          },
+        });
+        web3auth.configureAdapter(openloginAdapter);
+
+
+        setWeb3auth(web3auth);
+
+        await web3auth.init();
+        if (web3auth.connected) {
+          setProvider(web3auth.provider);
+        };
+
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const login = async () => {
+    if (!web3auth) {
+      console.log("web3auth not initialized yet");
+      // @ts-ignore
+      window.Telegram.WebApp.showAlert("web3auth not initialized yet");
+      return;
+    }
+    try {
+      // const web3authProvider = await web3auth.connectTo(WALLET_ADAPTERS.OPENLOGIN, {
+      //   mfaLevel: "default", // Pass on the mfa level of your choice: default, optional, mandatory, none
+      //   loginProvider: "twitter", // Pass on the login provider of your choice: google, facebook, discord, twitch, twitter, github, linkedin, apple, etc.
+      // });
+      const web3authProvider = await web3auth.connectTo(WALLET_ADAPTERS.OPENLOGIN, {
+        loginProvider: "email_passwordless",
+        extraLoginOptions: {
+          login_hint: email, // email to send the OTP to
+        },
+      });
+      setProvider(web3authProvider);
+    } catch (e) {
+      console.error(e);
+      // @ts-ignore
+      window.Telegram.WebApp.showAlert(e.message);
+    }
+  };
+
+  const authenticateUser = async () => {
+    if (!web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    const idToken = await web3auth.authenticateUser();
+    console.log(idToken);
+  };
+
+  const getUserInfo = async () => {
+    if (!web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    const user = await web3auth.getUserInfo();
+    console.log(user);
+  };
+
+  const logout = async () => {
+    if (!web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    await web3auth.logout();
+    setProvider(null);
+  };
+
+  const getChainId = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const chainId = await rpc.getChainId();
+    console.log(chainId);
+  };
+  const getAccounts = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const address = await rpc.getAccounts();
+    console.log(address);
+    // @ts-ignore
+    console.log({telegram: window.Telegram})
+    // @ts-ignore
+    if (window.Telegram) {
+      // @ts-ignore
+      window.Telegram?.WebApp?.showAlert("Address: " + address);
+    }
+  };
+
+  const getBalance = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const balance = await rpc.getBalance();
+    console.log(balance);
+  };
+
+  const sendTransaction = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const receipt = await rpc.sendTransaction();
+    console.log(receipt);
+  };
+
+  const signMessage = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const signedMessage = await rpc.signMessage();
+    console.log(signedMessage);
+  };
+
+  const getPrivateKey = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const privateKey = await rpc.getPrivateKey();
+    console.log(privateKey);
+  };
+
+  const loggedInView = (
+      <>
+        <div className="flex-container">
+          <div>
+            <button onClick={getUserInfo} className="card">
+              Get User Info
+            </button>
+          </div>
+          <div>
+            <button onClick={authenticateUser} className="card">
+              Get ID Token
+            </button>
+          </div>
+          <div>
+            <button onClick={getChainId} className="card">
+              Get Chain ID
+            </button>
+          </div>
+          <div>
+            <button onClick={getAccounts} className="card">
+              Get Accounts
+            </button>
+          </div>
+          <div>
+            <button onClick={getBalance} className="card">
+              Get Balance
+            </button>
+          </div>
+          <div>
+            <button onClick={signMessage} className="card">
+              Sign Message
+            </button>
+          </div>
+          <div>
+            <button onClick={sendTransaction} className="card">
+              Send Transaction
+            </button>
+          </div>
+          <div>
+            <button onClick={getPrivateKey} className="card">
+              Get Private Key
+            </button>
+          </div>
+          <div>
+            <button onClick={logout} className="card">
+              Log Out
+            </button>
+          </div>
         </div>
+
+        <div id="console" style={{ whiteSpace: "pre-line" }}>
+          <p style={{ whiteSpace: "pre-line" }}>Logged in Successfully!</p>
+        </div>
+      </>
+  );
+
+
+
+  const unloggedInView = (
+      <>
+        <label>
+          E-mail:
+          <input type="text" value={email} onChange={(event) => setEmail(event.target.value)} />
+        </label>
+
+        <button onClick={login} className="card">
+          Login
+        </button>
+      </>
+  );
+
+  return (
+      <div className="container">
+        <h1 className="title">
+          <a target="_blank" href="http://web3auth.io/" rel="noreferrer">
+            Web3Auth
+          </a>
+          & NextJS Example
+        </h1>
+
+        <div className="grid">{provider ? loggedInView : unloggedInView}</div>
+
+        <footer className="footer">
+          <a href="https://github.com/Web3Auth/web3auth-pnp-examples/" target="_blank" rel="noopener noreferrer">
+            Source code
+          </a>
+        </footer>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  );
 }
